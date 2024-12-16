9ac21b1b548b37f71a25cc8dae535d5a4854f66c
idriss <idriss.said@epitech.eu>
2024-12-16 08:56:54 +0100

    Add bdd

--- copyme/app/api/form/route.ts
+++ copyme/app/api/form/route.ts
@@ -1,27 +1,33 @@
 /** @format */
 
-
-import { NextRequest,NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import connectDB from '@/lib/connectDB'
+import Form from '@/Model/Form';
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  const { name, email, work, otherWork } = await req.json();
+	const { name, email, work, otherWork } = await req.json();
 
-  // Basic validation
-  if (!name || !email || !work) {
-    return NextResponse.json({ error: 'All required fields must be filled out' }, { status: 400 });
-  }
+	// Basic validation
+	if (!name || !email || !work) {
+		return NextResponse.json({ error: 'All required fields must be filled out' }, { status: 400 });
+	}
 
-  try {
-    // Process the data
-    // For example, save the data to a database or send an email notification
-    // This is a placeholder for database interaction (e.g., MongoDB, Prisma, etc.)
+	try {
+		await connectDB();
 
-    console.log('Form Data:', { name, email, work, otherWork });
+		const form = new Form({
+			name: name,
+      email: email,
+      work: work,
+      otherWork: otherWork,
+		});
+		await form.save();
+		console.log('Form Data:', { name, email, work, otherWork });
 
-    // Respond with success
-    return NextResponse.json({ message: 'Form submitted successfully' }, { status: 200 });
-  } catch (error) {
-    console.error('Error processing form:', error);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-  }
+		// Respond with success
+		return NextResponse.json({ message: 'Form submitted successfully' }, { status: 200 });
+	} catch (error) {
+		console.error('Error processing form:', error);
+		return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+	}
 }
