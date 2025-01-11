92c0836f3e14f34fef8d3c78bba1b4ac1a806180
mpJunot <junot-hery-nantenaina.montpre@epitech.eu>
2025-01-12 00:00:11 +0800

    fix(installer): better installer

--- install.sh
+++ install.sh
@@ -5,27 +5,27 @@
 # pip is required to install the packages
 if ! [ -x "$(command -v pip)" ]; then
     echo "Error: pip is not installed." >&2
-    exit 1
-fi
+    # install pip if not installed
 
-# install pip if not installed
-unameOut=$(uname -a)
-case "${unameOut}" in
-    *Microsoft*)     OS="WSL";; #must be first since Windows subsystem for linux will have Linux in the name too
-    *microsoft*)     OS="WSL2";; #WARNING: My v2 uses ubuntu 20.4 at the moment slightly different name may not always work
-    Linux*)     OS="Linux";;
-    Darwin*)    OS="Mac";;
-    CYGWIN*)    OS="Cygwin";;
-    MINGW*)     OS="Windows";;
-    *Msys)     OS="Windows";;
-    *)          OS="UNKNOWN:${unameOut}"
-esac
+    unameOut=$(uname -a)
+    case "${unameOut}" in
+        *Microsoft*)     OS="WSL";; #must be first since Windows subsystem for linux will have Linux in the name too
+        *microsoft*)     OS="WSL2";; #WARNING: My v2 uses ubuntu 20.4 at the moment slightly different name may not always work
+        Linux*)     OS="Linux";;
+        Darwin*)    OS="Mac";;
+        CYGWIN*)    OS="Cygwin";;
+        MINGW*)     OS="Windows";;
+        *Msys)     OS="Windows";;
+        *)          OS="UNKNOWN:${unameOut}"
+    esac
+    echo "Installing pip on $OS"
 
-# on linux and macos
-if [ $OS == "Linux" ] || [ $OS == "Mac" ]; then
-    python3 -m ensurepip --upgrade
-elif [ $OS == "Windows" ]; then # on windows
-    py -m ensurepip --upgrade
+    # on linux and macos
+    if [ $OS == "Linux" ] || [ $OS == "Mac" ]; then
+        python3 -m ensurepip --upgrade
+    elif [ $OS == "Windows" ]; then # on windows
+        py -m ensurepip --upgrade
+    fi
 fi
 
 # Install the required packages
