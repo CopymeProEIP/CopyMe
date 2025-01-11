30cef409ab5c9a88037756fb00d49c64a59cb087
mpJunot <junot-hery-nantenaina.montpre@epitech.eu>
2025-01-11 23:59:18 +0800

    fix(installer): better installerclear

--- install.sh
+++ install.sh
@@ -1,8 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Install the required packages with requirements.txt
 
 # pip is required to install the packages
+if ! [ -x "$(command -v pip)" ]; then
+    echo "Error: pip is not installed." >&2
+    exit 1
+fi
+
+# install pip if not installed
+unameOut=$(uname -a)
+case "${unameOut}" in
+    *Microsoft*)     OS="WSL";; #must be first since Windows subsystem for linux will have Linux in the name too
+    *microsoft*)     OS="WSL2";; #WARNING: My v2 uses ubuntu 20.4 at the moment slightly different name may not always work
+    Linux*)     OS="Linux";;
+    Darwin*)    OS="Mac";;
+    CYGWIN*)    OS="Cygwin";;
+    MINGW*)     OS="Windows";;
+    *Msys)     OS="Windows";;
+    *)          OS="UNKNOWN:${unameOut}"
+esac
+
+# on linux and macos
+if [ $OS == "Linux" ] || [ $OS == "Mac" ]; then
+    python3 -m ensurepip --upgrade
+elif [ $OS == "Windows" ]; then # on windows
+    py -m ensurepip --upgrade
+fi
 
 # Install the required packages
 pip install -r requirements.txt
