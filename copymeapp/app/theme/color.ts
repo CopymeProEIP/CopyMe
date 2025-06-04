889baed34b2e2cdb0de47da2259c5524ba4dc6fd
idriss <idriss.said@epitech.eu>
2025-06-04 21:17:58 +0200

    Feat: const theme

---
+++ copymeapp/app/theme/color.ts
@@ -0,0 +1,19 @@
+/** @format */
+
+export default {
+	colors: {
+		primary: '#F8F9FA',
+		secondary: '#E8E8E8',
+		foreground: '#FFFFFF',
+		accent: '#FF7F3F',
+		hoverAccent: '#E36414',
+		error: '#E63946',
+		success: '#4CAF50',
+		divider: '#E0E0E0',
+		textPrimary: '#1E1E1E',
+		textSecondary: '#4F4F4F',
+		placeholder: '#A0A0A0',
+		textDisabled: '#BDBDBD',
+		border: '#DEE2E6',
+	},
+};
